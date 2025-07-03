b1861b6e032dd9be8c754164a9f34acdccfd71ef
o-sallam <omar.sallam.dev@gmail.com>
2025-07-03 14:42:02 +0200

    deploy

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module go-api-test
 
 go 1.24.4
+
+require github.com/tdewolff/minify/v2 v2.23.8
+
+require github.com/tdewolff/parse/v2 v2.8.1 // indirect
--- utils/html.go
+++ utils/html.go
@@ -1,13 +1,29 @@
 package utils
 
-import "strings"
+import (
+	"github.com/tdewolff/minify/v2"
+	cssmin "github.com/tdewolff/minify/v2/css"
+	htmlmin "github.com/tdewolff/minify/v2/html"
+)
 
-// MinifyHTML removes newlines and extra spaces between tags
+// MinifyHTML uses tdewolff/minify for HTML minification
 func MinifyHTML(input string) string {
-	return strings.Join(strings.Fields(input), " ")
+	m := minify.New()
+	m.Add("text/html", &htmlmin.Minifier{})
+	minified, err := m.String("text/html", input)
+	if err != nil {
+		return input // fallback to original if error
+	}
+	return minified
 }
 
-// MinifyCSS removes newlines and extra spaces from CSS
+// MinifyCSS uses tdewolff/minify for CSS minification
 func MinifyCSS(input string) string {
-	return strings.Join(strings.Fields(input), " ")
+	m := minify.New()
+	m.Add("text/css", &cssmin.Minifier{})
+	minified, err := m.String("text/css", input)
+	if err != nil {
+		return input // fallback to original if error
+	}
+	return minified
 }
